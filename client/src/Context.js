d06aba9c69a6c45e65609397a7394a7597c780de
Search-Pankaj <pankajmay97@gmail.com>
2021-06-20 10:49:19 +0530

    deployed website ,info stire in Readme

--- client/src/Context.js
+++ client/src/Context.js
@@ -5,7 +5,7 @@ import Peer from "simple-peer";
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io("http://localhost:5000/");
+const socket = io("https://konect-chat.herokuapp.com/");
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
